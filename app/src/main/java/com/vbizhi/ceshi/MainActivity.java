6200249ff496867370d4ad5c5b67b607db43e244
kazenari <kazenarihb@yahoo.co.jp>
2018-08-27 00:28:10 +0800

    提交测试

--- app/src/main/java/com/vbizhi/ceshi/MainActivity.java
+++ app/src/main/java/com/vbizhi/ceshi/MainActivity.java
@@ -1,13 +1,28 @@
 package com.vbizhi.ceshi;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mBtnEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mBtnEditText = findViewById(R.id.btn_edittext);
+        mBtnEditText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //跳转到EditText页面
+                Intent intent = new Intent(MainActivity.this,EditTextActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
